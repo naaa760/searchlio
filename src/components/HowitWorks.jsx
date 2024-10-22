74b2bd5ec14b593c00c20be8eeaa3876d4a69bd7
nehhhhha6 <n0370525@gmail.com>
2024-10-22 06:52:09 +0530

    message

---
+++ src/components/HowitWorks.jsx
@@ -0,0 +1,50 @@
+// /app/HowItWorks.tsx or /pages/HowItWorks.tsx
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const HowItWorks = () => {
+  return (
+    <div>
+      <h1>How It Works</h1>
+      <p>
+        Welcome to our platform! Here iss a quick breakdown of how it works:
+      </p>
+
+      <p>
+        Privacy-first search engines prioritize user anonymity by minimizing
+        data collection and avoiding tracking. They ensure secure browsing
+        without targeted advertising or personal data leaks.
+      </p>
+
+      <p>
+        Companies collect user data through search engines using tracking
+        cookies and search logs, creating profiles for targeted ads while
+        raising privacy concerns.
+      </p>
+
+      <p>
+        To address privacy concerns, companies should use privacy-focused tools,
+        enhance transparency, give users control over their data, ensure
+        regulatory compliance, practice data minimization, and conduct regular
+        audits.
+      </p>
+
+      <h2>Explore & Engage</h2>
+      <p>
+        Once everything is set up, dive into the platform and start exploring.
+        Browse through the resources, participate in discussions, and
+        collaborate with others to get the most out of your experience.
+      </p>
+
+      <h2>Feedback & Improvements</h2>
+      <p>
+        We are constantly improving, so your feedback is invaluable. Let us know
+        what you love and what can be enhanced.
+      </p>
+
+      <p>That is it! You are all set to get the most out of our platform.</p>
+    </div>
+  );
+};
+
+export default HowItWorks;
